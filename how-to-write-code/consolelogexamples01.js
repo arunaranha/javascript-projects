da09902d2212d28a83701b3ad332ff88b47a95cf
Arun Aranha <aaranha@yahoo.com>
2023-09-11 16:54:37 -0500

    DataExercises

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -5,3 +5,25 @@ console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
 console.log('coding rocks')
+console.log('three')
+console.log("3.3")
+console.log(Number('3 3'));
+console.log(Number('three'));
+console.log(Number('3'))
+console.log('3.34')
+console.log(typeof "17")
+console.log(typeof "3.2")
+console.log(typeof '3 3')
+let day = "Thursday";
+day = 32;
+day = 19;
+console.log(day);
+const appName = "Editor Name";
+console.log(appName);
+let x = 16 -2 * 5 / 3 + 1
+console.log(x);
+console.log(1 + 5 % 3)
+let y = (2 ** 2 ** 3 * 3)
+console.log(y)
+const input = require('readline-sync');
+
